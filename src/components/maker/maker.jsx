ec2ea82a708ef50a683544d9d8d173cfd308656d
Choi Eun Jeong <qhdrn67@naver.com>
2022-07-28 00:19:49 +0900

    feat : add CardAddForm component

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -52,11 +52,13 @@ const Maker = ({ authService }) => {
     });
   }, []);
 
+  const addCard = (card) => setCards((prev) => [...prev, card]);
+
   return (
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor cards={cards} />
+        <Editor cards={cards} addCard={addCard} />
         <Preview cards={cards} />
       </div>
       <Footer />
